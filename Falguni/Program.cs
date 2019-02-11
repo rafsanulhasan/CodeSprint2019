f01727d73a09cb6d8434c7918dfebb4aa4213604
rafsanulhasan <rafsanulhasan@RHPC-MSI>
2019-02-11 23:12:19 +0600

    Solved Problem

--- Falguni/Program.cs
+++ Falguni/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Falguni
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			for (var i = 0; i <= 100; i++)
+			{
+				if (i > 2)
+				{
+					var cond1 = i % 3 == 0;
+					var cond2 = i % 5 == 0;
+					var outout = string.Empty;
+					if (cond1 && cond2)
+						outout = "Bangladesh";
+					else if (cond1)
+						outout = "Bangla";
+					else if (cond2)
+						outout = "Desh";
+					else
+						outout = $"{i}";
+					Console.WriteLine(outout);
+				}
+				else
+				{
+					Console.WriteLine(i);
+				}
+			}
+
+			Console.ReadLine();
 		}
 	}
 }
